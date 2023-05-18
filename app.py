c98398c9858a782a56755e45ceacbbd8feb71056
mihirar <mihiraraveeti@gmail.com>
2023-05-18 15:20:13 -0400

    Instructions & Download

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-#PDF AI - V1 on Streamlit
-
 #Imports
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -17,17 +15,23 @@ load_dotenv(find_dotenv())
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 #Styling 
-
 st.set_page_config(page_title="Ask your PDF")
 st.title("Ask your PDF")
-st.markdown('## Welcome to Ask your PDF! Upload your PDF and ask questions about its content.')
+
+#Instructions
+instructions = """
+## Instructions
+1. Upload a PDF file using the file uploader below.
+2. Wait for the system to extract text from your PDF.
+3. Once the text is extracted, you can ask questions related to the content of the PDF.
+"""
+st.markdown(instructions)
 
 pdf = st.file_uploader("Upload your PDF here", type="pdf")
 
 if pdf is not None:
 
     #Extract Text
-
     with st.spinner('Extracting text from PDF...'):
         pdf_reader = PdfReader(pdf)
         text = ""
@@ -39,7 +43,6 @@ if pdf is not None:
             st.error('Failed to extract text from the PDF. Please try another file.')
 
     #Split Text Into Chunks
-
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
@@ -49,7 +52,6 @@ if pdf is not None:
     chunks = text_splitter.split_text(text)
 
     #Embeddings
-
     with st.spinner('Generating embeddings...'):
         with get_openai_callback() as cb:
             embeddings = OpenAIEmbeddings()
@@ -57,7 +59,6 @@ if pdf is not None:
     st.success('Embeddings generated successfully.')
 
     #LLM Usage
-
     question = st.text_input("Ask a question about your PDF")
     if question:
         with st.spinner('Finding answers...'):
@@ -69,8 +70,8 @@ if pdf is not None:
 
         if response:
             st.markdown(f'## Answer: \n {response}')
+            st.download_button("Download Answer", data=response, file_name="answer.txt")
         else:
             st.error('Unable to find an answer to your question. Please try asking in a different way.')
-
 else:
     st.info('Please upload a PDF file.')
